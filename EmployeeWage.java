bcbdca54e69990da186e6ec3bfdee9f4a9cfd238
sp-1cell <sushant6247@gmail.com>
2023-02-23 11:18:08 +0530

    CODE FOR CALCULATE WAGES TILL A CONDITION OF A TOTAL WORKING HOURS

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,17 +3,21 @@ package EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEEWAGE PROGRAME");
-        class Employee_UC5 {
+        class Employee_UC6 {
             public static final int IsFullTime = 1;
             public static final int IsPartTime = 2;
             public static final int EmployeRatePerHour = 20;
             public static final int NoWorkingDays = 20;
+            public static final int HourInMonth = 100;
 
-            public  void main(String[] Args) {
+            public void main(String[] Args) {
                 int EmployeWages = 0;
-                int EmployeHour = 0;
+                int TotalEmployeHour = 0;
                 int TotalEmployeWage = 0;
-                for (int day = 0; day < NoWorkingDays; day++) {
+                int TotalWorkingDays = 0;
+                while (TotalWorkingDays < NoWorkingDays && TotalEmployeHour <= HourInMonth) {
+                    int EmployeHour = 0;
+                    TotalWorkingDays++;
                     int EmployeCheck = (int) Math.floor(Math.random() * 10) % 3;
                     switch (EmployeCheck) {
                         case IsFullTime:
@@ -25,6 +29,7 @@ public class EmployeeWage {
                         default:
                             EmployeHour = 0;
                     }
+                    TotalEmployeHour += EmployeHour;
                     EmployeWages = EmployeHour * EmployeRatePerHour;
                     TotalEmployeWage += EmployeWages;
                     System.out.println("Employewage: " + EmployeWages);
