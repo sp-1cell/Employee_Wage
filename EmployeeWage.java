f7e509863b5504df67df8507521c7f46c290c57d
sp-1cell <sushant6247@gmail.com>
2023-02-23 11:01:21 +0530

    CODE FOR SWITCH CASE

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,26 +2,32 @@ package EmployeeWage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
-        public static final int IsFullTime = 1;
-        public static final int IsPartTime = 2;
-        public static final int EmployeRatePerHour = 20;
+        System.out.println("WELCOME TO EMPLOYEEWAGE PROGRAME");
+        class Employee_UC4 {
+            public static final int IsFullTime = 1;
+            public static final int IsPartTime = 2;
+            public static final int EmployeRatePerHour = 20;
 
-        public static void main(String[] Args) {
-            int EmployeWages = 0;
-            int EmployeHour = 0;
-            int EmployeCheck = (int) Math.floor(Math.random() * 10) % 3;
-            switch (EmployeCheck) {
-                case IsFullTime:
-                    EmployeHour = 8;
-                    break;
-                case IsPartTime:
-                    EmployeHour = 4;
-                    break;
-                default:
-                    EmployeHour = 0;
+
+            public void main(String[] Args) {
+                int EmployeWages = 0;
+                int EmployeHour = 0;
+                int EmployeCheck = (int) Math.floor(Math.random() * 10) % 3;
+                switch (EmployeCheck) {
+                    case IsFullTime:
+                        EmployeHour = 8;
+                        break;
+                    case IsPartTime:
+                        EmployeHour = 4;
+                        break;
+                    default:
+                        EmployeHour = 0;
+                }
+                EmployeWages = EmployeHour * EmployeRatePerHour;
+                System.out.println("Employewage: " + EmployeWages);
             }
-            EmployeWages = EmployeHour * EmployeRatePerHour;
-            System.out.println("Employewage: " + EmployeWages);
+
+        }
 
 
     }
