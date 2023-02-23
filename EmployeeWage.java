76d0b90c8c2a1db23ad6d5326509e5d67fb1a4be
sp-1cell <sushant6247@gmail.com>
2023-02-23 10:06:09 +0530

    ADDED CODE FOR PART TIME

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,5 +3,15 @@ package EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE PROGRAME");
+        int isfulltime = 1;
+        int employeerateperhour = 20;
+        int employeewage = 0;
+        int employeehour = 0;
+        double empcheck = Math.floor(Math.random() * 10 % 2);
+        if (empcheck == isfulltime)
+            employeehour = 8;
+        else employeehour = 0;
+        employeewage = employeehour * employeerateperhour;
+        System.out.println("EmployeeWage:" + employeewage);
     }
 }
