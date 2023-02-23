668924371aabab47e87f8aed1e63fe467e897161
sp-1cell <sushant6247@gmail.com>
2023-02-23 09:58:18 +0530

    ADDED EMP ATTENDANCE

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,5 +3,11 @@ package EmployeeWage;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("WELCOME TO EMPLOYEE WAGE PROGRAME");
+        int IsFullTime = 1;
+        double EmpCheck = Math.floor(Math.random() * 10) % 2;
+        if (EmpCheck == IsFullTime)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Not Present");
     }
 }
